8337abc8555210bb6eb4579c4049225b54b2a286
LienVNH <lien.vannieuwenhuyse1@student.arteveldehs.be>
2025-06-15 07:03:31 +0200

    Vercel build fixing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import "react-native-url-polyfill/auto";
-import { Slot } from "expo-router";
-import { AuthProvider } from "./context/AuthContext";
+// import "react-native-url-polyfill/auto";
+// import { Slot } from "expo-router";
+// import { AuthProvider } from "./context/AuthContext";
 
-export default function App() {
-  return (
-    <AuthProvider>
-      <Slot /> 
-    </AuthProvider>
-  );
-}
+// export default function App() {
+//   return (
+//     <AuthProvider>
+//       <Slot /> 
+//     </AuthProvider>
+//   );
+// }
+import "expo-router/entry";
